1b746514ee246e0768ebb0c5fade15f6a09ad5a7
Johnathon Selstad <makeshifted@gmail.com>
2021-06-08 21:51:58 -0700

    Minor Formatting and Cleanup

--- test/server.py
+++ test/server.py
@@ -11,16 +11,16 @@ Handler = http.server.SimpleHTTPRequestHandler
 
 Handler.extensions_map={
     '.manifest': 'text/cache-manifest',
-    '.html': 'text/html',
-    '.png': 'image/png',
-    '.jpg': 'image/jpg',
-    '.svg':	'image/svg+xml',
-    '.css':	'text/css',
-    '.js':	'application/x-javascript',
-    '.json': 'application/json',
-    '.xml': 'application/xml',
-    '.wasm': 'application/wasm',
-    '' : 'application/octet-stream', # Default
+    '.html'    : 'text/html',
+    '.png'     : 'image/png',
+    '.jpg'     : 'image/jpg',
+    '.svg'     : 'image/svg+xml',
+    '.css'     : 'text/css',
+    '.js'      : 'application/x-javascript',
+    '.json'    : 'application/json',
+    '.xml'     : 'application/xml',
+    '.wasm'    : 'application/wasm',
+    ''         : 'application/octet-stream', # Default
 }
 
 httpd = socketserver.TCPServer(("", PORT), Handler)
